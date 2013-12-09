f10800542055e4ca75ee430e2576ce8ca3c4c1cf
vincent <vincentvo@github.com>
2013-12-09 04:05:46 -0800

    Finish signup

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'jquery-rails', '3.0.4'
 gem 'turbolinks', '1.1.1'
 gem 'jbuilder', '1.0.2'
 gem 'bcrypt-ruby', '3.1.2'
+gem 'factory_girl_rails', '4.2.1'
+
 
 
 group :doc do
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 TwitterApp::Application.routes.draw do
-  get 'users/new'
-
+  resources :users
   match '/signup', to: 'users#new', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
